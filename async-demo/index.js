5c400bd8c3b46b1fcd0249837462abe82851863e
Chandler Fang <chandlerfang@gmail.com>
2018-05-21 22:48:37 +1000

    Promise.

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,18 +1,23 @@
-console.log('Before');
-getUser(1, user => {
-
-        console.log('User', user)
-        // Get the repositories.
-        getRepositories(user.githubUsername, repos =>
-            console.log('Repos', repos)
-        );
-    }
-);
-console.log('After');
-
 // Callbacks
 // Promises
 // Async/await
+
+console.log('Before');
+getUser(1, displayUser);
+console.log('After');
+
+function displayUser(user) {
+    // Get the repositories.
+    getRepositories(user.githubUsername, displayRepos);
+}
+
+function displayRepos(repos) {
+    getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits) {
+    console.log(commits);
+}
 
 function getUser(id, callback) {
     setTimeout(
---
+++ async-demo/promise.js
@@ -0,0 +1,13 @@
+const p = new Promise((resolve, reject) => {
+    // Kick off some async work.
+    // ...
+    setTimeout(() => { 
+        // resolve(1); 
+        reject(new Error('message'));
+    }, 2000);
+    // reject(new Error('message'));
+});
+
+p
+ .then(result => console.log('Result', result))
+ .catch(error => console.log('Error', error.message));
