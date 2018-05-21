ece9b596ff1e2eb5e8ec5c9f424a292268fc43ef
Chandler Fang <chandlerfang@gmail.com>
2018-05-22 00:30:52 +1000

    Promise.

--- async-demo/promise-api.js
+++ async-demo/promise-api.js
@@ -3,3 +3,28 @@ Promise.resolve({ id: 1, })
 
 Promise.reject(new Error('reason for rejection...'))
 .catch(result => console.log(result));
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('Async operation 1...');
+        resolve(1);
+        // reject(new Error('because something failed.'));
+    }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 2...');
+        resolve(2);
+    }, 2000);
+});
+
+/*
+Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(error => console.log('Error', error.message));
+*/
+
+Promise.race([p1, p2])
+    .then(result => console.log(result))
+    .catch(error => console.log('Error', error.message));
